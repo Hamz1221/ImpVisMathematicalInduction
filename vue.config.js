4b222d75bdcb119c5f16dc345e5d928961411477
Hamzah <h4mz1221@gmail.com>
2022-02-02 13:33:57 +0000

    First commit

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    publicPath:'./',
+    pages:{
+        index:{
+            entry:'src/main.js',
+            title:'Index'
+        },
+            page_1:{
+                entry:'src/Page 1/main.js',
+                title:'Page 1',
+                
+            },
+            page_2:{
+                entry:'src/Page 2/main.js',
+                title:'Page 2',
+                
+            },
+    }
+}
